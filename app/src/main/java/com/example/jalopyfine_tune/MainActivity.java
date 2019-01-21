d800606f8561a654f6ec9cdb5101f2d839a26ab0
BABAR <babarmunawar786@gmail.com>
2019-01-21 18:01:55 +0500

    This is Final year project

---
+++ app/src/main/java/com/example/jalopyfine_tune/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.jalopyfine_tune;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button signup_btn,signin_btn;
+        signin_btn=findViewById(R.id.SignInbtn);
+        signup_btn=findViewById(R.id.SignUpbtn);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i_signup=new Intent(MainActivity.this,SignUp.class);
+                startActivity(i_signup);
+            }
+        });
+
+        signin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i_signin=new Intent(MainActivity.this,SignIn.class);
+                startActivity(i_signin);
+
+            }
+        });
+    }
+}
